eaa7d088820346c0f417f14e15eff49548195e0d
sagunji <karanjit.sagun01@gmail.com>
2021-05-10 08:23:50 +0545

    Toggle video & audio

--- src/Room.js
+++ src/Room.js
@@ -1,8 +1,44 @@
 import React, { useEffect, useState } from "react";
 import Participant from "./Participant";
 
-const Room = ({ roomName, room, handleLogout }) => {
+const Room = ({ roomName, room, handleLogout, handleMute, handleVideo }) => {
   const [participants, setParticipants] = useState([]);
+  const [isMute, setIsMute] = useState(false);
+  const [videoEnabled, isVideoEnabled] = useState(true);
+
+  const toggleMute = () => {
+    setIsMute(!isMute);
+    toggleAudio();
+  }
+
+  const toggleVideo = () => {
+    isVideoEnabled(!videoEnabled);
+    muteummuteVideo();
+  }
+
+  // Use attachng and detaching instead of enabling & disabling
+  const toggleAudio = () => {
+    var localParticipant = room.localParticipant;
+    localParticipant.audioTracks.forEach((audioTrack) => {
+    if (audioTrack.isEnabled === true ) {
+      audioTrack.track.disable();
+    } else {
+      audioTrack.track.enable();
+    }
+    });
+  }
+
+  // Use attachng and detaching instead of enabling & disabling 
+  const muteummuteVideo = () => {
+    var localParticipant = room.localParticipant;
+    localParticipant.videoTracks.forEach((videoTrack) => {
+      if (videoTrack.isTrackEnabled === true ) {
+        videoTrack.track.disable();
+      } else {
+        videoTrack.track.enable();
+      }
+    });
+  }
 
   useEffect(() => {
     const participantConnected = (participant) => {
@@ -19,6 +55,8 @@ const Room = ({ roomName, room, handleLogout }) => {
     room.on("participantDisconnected", participantDisconnected);
     room.participants.forEach(participantConnected);
     return () => {
+      // dont know its effect
+
       room.off("participantConnected", participantConnected);
       room.off("participantDisconnected", participantDisconnected);
     };
@@ -31,7 +69,12 @@ const Room = ({ roomName, room, handleLogout }) => {
   return (
     <div className="room">
       <h2>Room: {roomName}</h2>
-      <button onClick={handleLogout}>Log out</button>
+      <div className="action-store">
+        <button onClick={handleLogout}>Log out</button>
+        <button className="child" onClick={toggleMute}>{isMute? "Turn audio on" : "Turn audio off"}</button>
+        <button className="child2" onClick={toggleVideo}>{videoEnabled? "Turn Off Video" : "Turn On Video"}</button>
+      </div>
+      
       <div className="local-participant">
         {room ? (
           <Participant
